7be7f43dd08bb2e069e498ed69ed9e195d935a1b
John Howard <orthopteroid@gmail.com>
2017-08-30 11:04:50 -0700

    code and doc reorgs for clarity

--- main.cpp
+++ main.cpp
@@ -27,8 +27,6 @@ void write_s16LE(ostream &stream, const float &value)
 {
     int16_t s16 = (uint16_t)((float)INT16_MAX * value);
     stream.write(reinterpret_cast<const char*>(&s16), sizeof(int16_t));
-    //stream.write(reinterpret_cast<const char*>(&value), sizeof(float));
-    //stream << value << '\n';
 }
 
 float read_s16LE(istream &stream)
@@ -56,17 +54,18 @@ int main()
         {
             ofstream outFile;
             outFile.open("out.bin", ios::binary);
-            PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementEncoder elem;
 
             bool q[] = {1,0,1,0,1,0};
-            elem.encode(
+
+            PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementEncoder encoder;
+            encoder.encode(
                 [&outFile](float v) { write_s16LE( outFile, v ); },
                 [&q](void) -> bool* { static int i = 0; return i < 6 ? &q[i++] : nullptr; }
             );
 
             outFile.close();
 
-            //system("/usr/bin/aplay -f cd out.bin"); // enable to play audio!
+            //system("/usr/bin/aplay -f cd -r 22050 out.bin"); // enable to play audio!
         }
 
         // read and decode
@@ -74,20 +73,18 @@ int main()
             ifstream inFile;
             inFile.open("out.bin", ios::binary);
 
-            PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementDecoder elem;
-            elem.reset();
-
             std::deque<bool> bitseq;
-            std::ostream &console = cout;
+            std::ostream &console = cout; // lower cout into local context for use in lambda
 
-            elem.decode(
+            PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementDecoder decoder;
+            decoder.decode(
                 [&inFile](void) -> float*
                 {
                     static float sample;
                     return inFile.eof() ? nullptr : &(sample = read_s16LE(inFile));
                 },
                 [&inFile,&bitseq](bool bit) -> void { bitseq.push_back(bit); },
-                [&console](int bit) -> void { static int x = 0; console << "error in bit " << bit << " " << x++ << std::endl; }
+                [&console](int bit) -> void { /* static int x = 0; console << "error in bit " << bit << " " << x++ << std::endl; */ } // todo: fix
             );
 
             // drop last bit as it is the transition from last element to the ending carrier
@@ -107,26 +104,24 @@ int main()
         {
             ofstream outFile;
             outFile.open("out.bin", ios::binary);
-            PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementEncoder elem;
 
             const char* message = "Hello World!";
 
-            std::ostream &console = cout;
+            std::ostream &console = cout; // lower cout into local context for use in lambda
 
-            elem.encode(
+            PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementEncoder encoder;
+            encoder.encode(
                 [&outFile](float v) { write_s16LE( outFile, v ); },
                 [&message,&console](void) -> bool*
                 {
                     static int i = 0;
                     static bool bit = false;
+
                     if(message[i/8] == 0) { return nullptr; }
                     bit = (bool)(message[i/8] & (1 << (i%8)));
 
                     console << bit << std::flush;
-
-                    i++;
-
-                    if(i%8 == 0) { console << ' ' << std::flush; }
+                    if(++i%8 == 0) { console << ' ' << std::flush; }
 
                     return &bit;
                 }
@@ -143,27 +138,23 @@ int main()
             ifstream inFile;
             inFile.open("out.bin", ios::binary);
 
-            PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementDecoder elem;
-            elem.reset();
+            std::ostream &console = cout; // lower cout into local context for use in lambda
 
-            std::ostream &console = cout;
-
-            elem.decode(
+            PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementDecoder decoder;
+            decoder.decode(
                     [&inFile](void) -> float*
                     {
-                        static float sample;
+                        static float sample; // simple hack that allows us to signal end-of-stream
                         return inFile.eof() ? nullptr : &(sample = read_s16LE(inFile));
                     },
                     [&console](bool bit) -> void
                     {
                         static int i = 0;
-                        static char c = 0;
-                        //if(bit) { c |= (char)(1 << (i%8)); }
-                        //if(++i%8 == 0) { console << c << std::flush; c = 0; }
+
                         console << bit << std::flush;
                         if(++i%8 == 0) { console << ' ' << std::flush; }
                     },
-                    [&console](int bit) -> void { /*console << "(E" << bit << ')';*/ }
+                    [&console](int bit) -> void { /*console << "(E" << bit << ')';*/ } // todo: fix
             );
 
             inFile.close();
@@ -180,26 +171,24 @@ int main()
         ofstream detailsFile;
         detailsFile.open("details.bin", ios::binary);
 
-        PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementDecoder elem;
-        elem.reset();
-
+        PSKOrange<element_cycles,carrier_cycles,freq,samplerate>::ElementDecoder decoder;
         while(!inFile.eof())
         {
             float sample = read_s16LE(inFile);
-            elem.process(sample);
+            decoder.process(sample);
 
             write_s16LE(detailsFile, sample);
-            write_s16LE(detailsFile, elem.inversionDetector.phase * M_1_PI);
+            write_s16LE(detailsFile, decoder.inversionDetector.phase * M_1_PI);
 
-            write_s16LE(detailsFile, elem.channelA.phase * M_1_PI);
-            write_s16LE(detailsFile, elem.channelA.iIntegrator);
-            write_s16LE(detailsFile, elem.channelA.qIntegrator);
-            write_s16LE(detailsFile, elem.channelA.squelch);
+            write_s16LE(detailsFile, decoder.channelA.phase * M_1_PI);
+            write_s16LE(detailsFile, decoder.channelA.iIntegrator);
+            write_s16LE(detailsFile, decoder.channelA.qIntegrator);
+            write_s16LE(detailsFile, decoder.channelA.squelch);
 
-            write_s16LE(detailsFile, elem.channelB.phase * M_1_PI);
-            write_s16LE(detailsFile, elem.channelB.iIntegrator);
-            write_s16LE(detailsFile, elem.channelB.qIntegrator);
-            write_s16LE(detailsFile, elem.channelB.squelch);
+            write_s16LE(detailsFile, decoder.channelB.phase * M_1_PI);
+            write_s16LE(detailsFile, decoder.channelB.iIntegrator);
+            write_s16LE(detailsFile, decoder.channelB.qIntegrator);
+            write_s16LE(detailsFile, decoder.channelB.squelch);
         }
 
         inFile.close();
